c2e17ca7464150bf095f2f7621a02541c3a730b7
duxpixels <duxwork0@gmail.com>
2023-04-26 22:52:26 +0200

    change title & icon & build

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import Countdown from "@/components/Countdown";
 import { useAppStore } from "../../lib/store";
 import Navbar from "@/components/Navbar";
 import { useState } from "react";
+import Head from "next/head";
 
 export default function Home() {
   const { isDark, workflowInMinutes, breakInMinutes, started, setStarted } =
@@ -10,6 +11,12 @@ export default function Home() {
 
   return (
     <main className={`${isDark && "dark"}`}>
+      <Head>
+        <title>Workflow time</title>
+        <meta name="description" content="start your workflow time with workflowtime." />
+        <meta name="keywords" content="workflow,promo,promodo,break,countdown" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
       <div className="w-screen h-screen flex flex-col bg-first-light dark:bg-first-dark text-slate-800 dark:text-slate-200">
         <div className="flex flex-col items-center justify-center h-full gap-10">
           <Countdown
